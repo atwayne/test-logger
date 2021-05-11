ecea4b558916503c435a19fbf708d4a9359576aa
Wayne Wang <wayne.wang@tetrapak.com>
2021-05-12 04:15:59 +0800

    implement test-logger

---
+++ src/TestLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace WayneLabs.TestLogger
+{
+    public class TestLogger<T> : ILogger<T>
+    {
+        public List<string> Messages { get; }
+        public List<Exception> Exceptions { get; }
+        public TestLogger()
+        {
+            Messages = new List<string>();
+            Exceptions = new List<Exception>();
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (exception != null)
+            {
+                Exceptions.Add(exception);
+            }
+
+            var result = formatter(state, exception);
+            Messages.Add(result);
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
